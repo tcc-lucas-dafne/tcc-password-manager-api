7634f1366bc560d2c5f681576d99e8f6dd88e028
lcsAndrade <lucascandradex@protonmail.com>
2023-11-29 11:59:28 -0300

    feat: add get user route

--- src/controller/users.js
+++ src/controller/users.js
@@ -51,7 +51,39 @@ const login = (req, res) => {
   })
 };
 
+const getUser = (req, res) => {
+  const { authorization } = req.headers;
+
+  try {
+    const token = authorization.split(' ')[1];
+
+    const decoded = jwt.decode(token, SECRET);
+    if (decoded && decoded.id) {
+      const userId = decoded.id;
+  
+      const text = `SELECT id, email FROM users WHERE id='${userId}'`;
+      pool.query(text, (error, results) => {
+        if (error) {
+          throw error;
+        }
+    
+        if (results.rowCount) {
+          const result = results.rows[0];    
+          res.status(200).json(result);
+        } else {
+          res.status(400).json({ "status": "error", "message": "not found" });
+        }
+      })
+    } else {
+      res.status(401).json({ "status": "error", "message": "invalid token" });
+    }
+  } catch (err) {
+    res.status(400).json({ "status": "error", "message": "invalid authorization" });
+  }
+}
+
 module.exports = {
   login, 
-  register
+  register,
+  getUser
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { login, register } = require('./controller/users');
+const { login, register, getUser } = require('./controller/users');
 const { getCredentials, saveCredential } = require('./controller/credentials');
 
 const routes = express.Router();
 
 routes.post('/user/login', login);
 routes.post('/user/register', register);
+routes.get('/user/', getUser);
 
 routes.get('/credential/:id', getCredentials);
 routes.post('/credential/', saveCredential);
