ee89a862dddf94bdbecc901c80d87562b2745280
lcsAndrade <lucascandradex@protonmail.com>
2023-11-30 08:57:35 -0300

    fix: credential url not being saved
    Co-authored-by: DafneM <moretti.dafne2@gmail.com>

--- src/controller/credentials.js
+++ src/controller/credentials.js
@@ -1,7 +1,5 @@
 const Pool = require('pg').Pool
 
-const SECRET = process.env.SECRET;
-
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
@@ -13,22 +11,26 @@ const pool = new Pool({
 const getCredentials = (req, res) => {
   const { id } = req.params;
 
-  const text = `SELECT * FROM credentials WHERE user_id = ${id}`;
+  if (Number.parseFloat(id)) {
+    const text = `SELECT * FROM credentials WHERE user_id = ${id}`;
 
-  pool.query(text, (error, results) => {
-    if (error) {
-      throw error;
-    }
-
-    res.status(200).json(results.rows)
-  })
+    pool.query(text, (error, results) => {
+      if (error) {
+        throw error;
+      }
+  
+      res.status(200).json(results.rows)
+    })
+  } else {
+    res.status(400).json({ "status": "error", "message": "invalid id" })
+  }
 };
 
 const saveCredential = (req, res) => {
-  const { id, name, email, password, username } = req.body;
+  const { id, name, username, url, email, password } = req.body;
 
-  const text = "INSERT INTO credentials(user_id, name, username, email, password) VALUES($1, $2, $3, $4, $5)";
-  const values = [id, name, username, email, password];
+  const text = "INSERT INTO credentials(user_id, name, username, url, email, password) VALUES($1, $2, $3, $4, $5, $6)";
+  const values = [id, name, username, url, email, password];
 
   pool.query(text, values, (error, results) => {
     if (error) {
