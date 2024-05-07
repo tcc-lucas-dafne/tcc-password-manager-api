825cc1555d8a1524286c696829eb39a1e2e10cd4
DafneM <moretti.dafne2@gmail.com>
2024-05-07 14:22:38 -0300

    #2 fix: add front URL on CORS policy
    Co-authored-by: nYCSTs <lucascandradex@protonmail.com>

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
 
+const corsOptions = {
+  origin: 'https://tcc-password-manager-n78y6rcv4-dafnes-projects-beb40209.vercel.app',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
